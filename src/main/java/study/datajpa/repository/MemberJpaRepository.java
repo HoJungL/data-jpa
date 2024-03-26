c2a47bf5cc7ecbb0f71e206af648ffbce065e9b0
HoJungL <ghaud5684@naver.com>
2024-03-26 20:07:19 +0900

    TEST에서 오래걸리는 이유 - gradle을 거쳐서 가기 때문에 오래걸림 ->  build를 gradle이 아닌, intellJ로 변경할것.
    Lombok을 적용하기 위해서는  반드시,setting -> annotation 에서 enable을 체킹 해줄것.

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,17 +5,43 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
     @PersistenceContext // 영속성 컨텍스트라는 것.
     private EntityManager em;
 
+    //저장
     public Member save(Member member) {
         em.persist(member);
         return member;
     }
+    
+    // 삭제
+    public void delete(Member member) {
+        em.remove(member);
+    }
 
+    // 전체 조회 일때는 ResultList
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member as m", Member.class)
+                .getResultList();
+    }
+
+    // 하나 조회
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    // 단건일때는 SingleResult
+    public long count() {
+        return em.createQuery("select count(m) from Member as m", Long.class)
+                .getSingleResult();
+    }
     // id 로 조회하기
     public Member find(Long id) {
         return em.find(Member.class, id);
